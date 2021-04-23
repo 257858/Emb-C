d8890071aa254af72cd06fc00e9c8b4281fb19c8
257858 <65301187+257858@users.noreply.github.com>
2021-04-23 21:04:42 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,53 +1,50 @@
 /**
  * @file project_main.c
- * @author Bharath.G ()
- * @brief Project to Blink an LED at 1000ms ON and 500 ms OFF Interval
+ * @author Vaishnavi DV
+ * @brief Project to Blink an LED when both the sensors output is 1
  * @version 0.1
  * @date 2021-04-23
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-#include "project_config.h"
 
-#include "user_utils.h"
-#include "blinky.h"
-
-/**
- * @brief Initialize all the Peripherals and pin configurations
- * 
- */
-void peripheral_init(void)
-{
-	/* Configure LED Pin */
-	DDRB |= (1 << DDB0);
-}
-
-void change_led_state(uint8_t state)
-{
-	LED_PORT = (state << LED_PIN);
-}
+#include "head.h"
 
 
 /**
  * @brief Main function where the code execution starts
  * 
  * @return int Return 0 if the program completes successfully
+ * @PORTB0 is output pin
+ * @PORTD0 is input pin
+ * @PORTD1 is input pin
  * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
  * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
  */
 int main(void)
 {
-	/* Initialize Peripherals */
-	peripheral_init();
+    DDRB|=(1<<PB0);//PB0 is set as output
+    DDRD&=~(1<<PD0);
+    PORTD|=(1<<PD0);
+    DDRD&=~(1<<PD1);
+    PORTD|=(1<<PD1);
+    while(1)
+    {
+        if(!(PIND &(1<<PD0)) & !(PIND &(1<<PD1))) //if both input is 1 then led is on
+        {
+            PORTB|=(1<<PB0);
+            //_delay_ms(2000);
+        }
+        else//else led is off
+        {
+            PORTB&=~(1<<PB0);
+            //_delay_ms(2000);
+        }
+    }
 
-	for(;;)
-	{
-        change_led_state(LED_ON);
-		delay_ms(LED_ON_TIME);
-		
-        change_led_state(LED_OFF);
-		delay_ms(LED_OFF_TIME);	
-	}
-	return 0;
+    return 0;
 }
+
+
+
