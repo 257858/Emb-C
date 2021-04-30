8b172adbd2b3ef270363829ae66fcf4ad334582c
257858 <65301187+257858@users.noreply.github.com>
2021-04-30 20:41:47 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,7 +1,7 @@
 /**
  * @file project_main.c
  * @author Vaishnavi DV
- * @brief Project to Blink an LED when person sits and turns the heater ON
+ * @brief Project to Blink an LED when person sits and turns on the heater and adjusts the temperatue
  * @version 0.1
  * @date 2021-04-23
  * 
@@ -9,42 +9,35 @@
  * 
  */
 
-#include "fuse.h"
 
 
 /**
  * @brief Main function where the code execution starts
  * 
  * @return int Return 0 if the program completes successfully
- * @PORTB0 is output pin
- * @PORTD0 is input pin
- * @PORTD1 is input pin
- * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
- * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
  */
+#include <avr/io.h>
+#include<util/delay.h>
+#include "act2.h"
 int main(void)
 {
-    DDRB|=(1<<PB0);//PB0 is set as output
-    DDRD&=~(1<<PD0);
-    PORTD|=(1<<PD0);
-    DDRD&=~(1<<PD1);
-    PORTD|=(1<<PD1);
+    act1();
+    InitADC();
+    uint16_t temp;
     while(1)
     {
         if(!(PIND &(1<<PD0)) & !(PIND &(1<<PD1))) //if both input is 1 then led is on
         {
             PORTB|=(1<<PB0);
-            //_delay_ms(2000);
+            _delay_ms(20);
         }
         else//else led is off
         {
             PORTB&=~(1<<PB0);
-            //_delay_ms(2000);
+            _delay_ms(20);
         }
+        temp=ReadADC(0);//read from channel 0
     }
 
     return 0;
 }
-
-
-
