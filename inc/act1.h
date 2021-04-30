f82683b35ec005e311919c85eecab85e58efcdf6
257858 <65301187+257858@users.noreply.github.com>
2021-04-30 20:37:47 +0530

    Update act1.h

--- inc/act1.h
+++ inc/act1.h
@@ -1,14 +1,6 @@
-#include <avr/io.h>
-/** \brief function: act1 defines all the ports needed for activity1
- * \return void
- *
- */
+#ifndef ACT1_H_INCLUDED
+#define ACT1_H_INCLUDED
 
-void act1()
-{
-    DDRB|=(1<<PB0);//PB0 is set as output
-    DDRD&=~(1<<PD0);
-    PORTD|=(1<<PD0);
-    DDRD&=~(1<<PD1);
-    PORTD|=(1<<PD1);
-}
+act1();
+
+#endif // ACT1_H_INCLUDED
